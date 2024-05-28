7acc0d6aaca040c214e99c400b708ee7c2a577af
Itskamikadze <rolbik1@o2.pl>
2024-05-28 21:56:01 +0200

    correction in code

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,4 @@
 def FizzBuzz(n):
-    result = []
     z = ["FizzBuzz"*(not x%15) or "Fizz"*(not x%3) or "Buzz"*(not x%5) or x for x in range(1, n+1)]
     print(z)
 result = int(input("Put a value: "))
